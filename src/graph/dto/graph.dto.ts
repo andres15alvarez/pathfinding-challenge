f9ec089b06e27d7e2e1e92ff6d0f48f4b9f01c27
Andres Alvarez <andres15alvarez@gmail.com>
2025-05-23 22:27:24 -0400

    Remove constructor and andd decorators

--- src/graph/dto/graph.dto.ts
+++ src/graph/dto/graph.dto.ts
@@ -1,46 +1,35 @@
-import { ArrayNotEmpty, IsNotEmpty } from 'class-validator';
+import {
+  ArrayNotEmpty,
+  IsNotEmpty,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
 import { EdgeDto } from './edge.dto';
+import { Expose, Type } from 'class-transformer';
 
 export class GraphDto {
   @ArrayNotEmpty()
+  @IsString({ each: true })
   nodes: string[];
 
   @IsNotEmpty()
   start: string;
 
+  @IsNotEmpty()
   end: string;
 
   @ArrayNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => EdgeDto)
   edges: EdgeDto[];
+}
 
-  constructor(nodes: string[], start: string, end: string, edges: EdgeDto[]) {
-    if (nodes.includes(start) === false) {
-      throw new Error(`Start node ${start} is not in the list of nodes`);
-    }
-    if (nodes.includes(end) === false) {
-      throw new Error(`End node ${end} is not in the list of nodes`);
-    }
-    const invalidEdge = edges.find((edge) => edge.from === edge.to);
-    if (invalidEdge) {
-      throw new Error(
-        `Edge from ${invalidEdge.from} to ${invalidEdge.to} is invalid`,
-      );
-    }
-    const invalidFrom = edges.find(
-      (edge) => nodes.includes(edge.from) === false,
-    );
-    if (invalidFrom) {
-      throw new Error(
-        `Edge from ${invalidFrom.from} is not in the list of nodes`,
-      );
-    }
-    const invalidTo = edges.find((edge) => nodes.includes(edge.to) === false);
-    if (invalidTo) {
-      throw new Error(`Edge to ${invalidTo.to} is not in the list of nodes`);
-    }
-    this.nodes = nodes;
-    this.start = start;
-    this.end = end;
-    this.edges = edges;
-  }
+export class OptimalPathDto {
+  @Expose()
+  @IsNotEmpty()
+  path: string[];
+
+  @Expose()
+  @IsNotEmpty()
+  cost: number;
 }
