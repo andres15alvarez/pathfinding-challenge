9f72dacf2aefe0d50762ee1cd88c5611f7cffe90
Andres Alvarez <andres15alvarez@gmail.com>
2025-05-23 22:28:17 -0400

    Create endpoint to get shortest path using dijsktra algorithm

--- src/graph/graph.controller.ts
+++ src/graph/graph.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { GraphService } from './graph.service';
+import { GraphDto, OptimalPathDto } from './dto/graph.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('graph')
 export class GraphController {
   constructor(private readonly graphService: GraphService) {}
+
+  @Post('shortest-path')
+  @ApiOperation({
+    summary: 'Find the shortest path in a graph',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The shortest path and its cost',
+    type: OptimalPathDto,
+  })
+  findShortestPath(@Body() graph: GraphDto): OptimalPathDto {
+    return this.graphService.findShortestPath(graph);
+  }
 }
--- src/graph/graph.service.ts
+++ src/graph/graph.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { GraphDto, OptimalPathDto } from './dto/graph.dto';
+import { Dijkstra } from './helpers/dijkstra';
 
 @Injectable()
-export class GraphService {}
+export class GraphService {
+  findShortestPath(graph: GraphDto): OptimalPathDto {
+    const dijkstra = new Dijkstra(graph.nodes, graph.edges);
+    const result = dijkstra.findShortestPath(graph.start, graph.end);
+    return result;
+  }
+}
